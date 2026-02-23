111c80dd8ef76251d8bedcb6734c7ecd73c13cde
Rzero6 <michaelreynoldk@gmail.com>
2026-02-24 01:02:26 +0700

    Refactor file loading paths in encoders and model loader to use BASE_DIR for improved portability

--- deployment/utils/encoders_loader.py
+++ deployment/utils/encoders_loader.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import pickle
+from pathlib import Path
 
+BASE_DIR = Path(__file__).parent
 @st.cache_resource
 def load_encoders():
-    with open("encoders/label_encoder.pkl", "rb") as f:
+    with open(BASE_DIR / "encoders" / "label_encoder.pkl", "rb") as f:
         label_encoder = pickle.load(f)
-    with open("encoders/onehot_encoder.pkl", "rb") as f:
+    with open(BASE_DIR / "encoders" / "onehot_encoder.pkl", "rb") as f:
         onehot_encoder = pickle.load(f)
-    with open("encoders/risk_encoder.pkl", "rb") as f:
+    with open(BASE_DIR / "encoders" / "risk_encoder.pkl", "rb") as f:
         risk_encoder = pickle.load(f)
     
     return label_encoder, onehot_encoder, risk_encoder
--- deployment/utils/model_loader.py
+++ deployment/utils/model_loader.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pickle
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
 
 @st.cache_resource
 def load_model():
-    with open("models/heart_attack_risk_classifier_xgb_model.pkl", "rb") as f:
+    with open(BASE_DIR / "models" / "heart_attack_risk_classifier_xgb_model.pkl", "rb") as f:
         model = pickle.load(f)
     
     return model
